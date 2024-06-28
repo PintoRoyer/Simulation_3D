ece6727d2ed1c790cb2b0974ee31d9c7d2c7f904
manonpinto <manonp2002@gmail.com>
2024-06-28 16:03:59 +0200

    add two limits files for DX = 500 m and DX = 250 m and add functions to plot clouds, water vapor, vertical and horizontal wind as 2D fields for different levels

--- plot_anim_3D.py
+++ plot_anim_3D.py
@@ -13,7 +13,7 @@ import json
 # import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.colors import LinearSegmentedColormap, TwoSlopeNorm
 
 from plots import Map
 from readers_3D import MesoNH, get_mesonh, get_time_index, index_to_lonlat
@@ -34,66 +34,202 @@ NO_ZOOM = (
 
 #PRESSURE
 def plot_pressure(mesonh, my_map, resol_dx, level, *, anim = False):
-
+    # # Limits for colorbars
+    with open(f"limits_3D/lim3D_{resol_dx}m.json", "r", encoding="utf-8") as file:
+        lim = json.loads(file.read())
+    lim_min = lim["pressure"][0]
+    lim_max = lim["pressure"][1]
+    
     var = mesonh.get_var("PABST", level=level)/100
     contourf = my_map.plot_contourf(
-            var, cmap="turbo",extend="both", levels=np.linspace(lim_min, lim_max, 100)
+            var, cmap="turbo", levels=np.linspace(lim_min, lim_max, 100)
         )
     
     level_value = int(mesonh.level[level])
     title = f"Simulation Méso-NH du 2022-08-18 à {time} (DX = {resol_dx} m)\nPression au niveau {level} (~{level_value} m) "
     title = my_map.set_title(title)
     
+    variable = "pressure"
+    label = "Pression (hPa)"
     
     if anim == False:
-        cbar = plt.colorbar(contourf, label="Pression (hPa)")
-        cbar.set_ticks(np.round(np.linspace(lim_min, lim_max, 8)))
+        cbar = plt.colorbar(contourf, label=label)
+        cbar.set_ticks(np.linspace(lim_min, lim_max, 8))
         plt.savefig(f"pressure_lvl{level}_{time}_dx{resol_dx}.png")
 
-    return contourf, title, time, lim_min, lim_max
+    return (contourf, title), time, lim_min, lim_max, variable, label
+
 
 #CLOUDS
 def sum_clouds(rct, rit, rgt, rst):
     """Add up different thickness of the condensed states of water."""
     return rct + rit + rgt + rst
 
+
 def plot_clouds(mesonh, my_map, resol_dx, level, *, anim = False):
-    var = mesonh.get_var("RCT", "RIT", "RGT", "RST", func=sum_clouds, level=level)
+    # # Limits for colorbars
+    with open(f"limits_3D/lim3D_{resol_dx}m.json", "r", encoding="utf-8") as file:
+        lim = json.loads(file.read())
+    lim_min = lim["clouds"][0]
+    lim_max = lim["clouds"][1]
+    
+    var = mesonh.get_var("RCT", "RIT", "RGT", "RST", func=sum_clouds, level=level)*1000
     contourf = my_map.plot_contourf(
-            var, cmap=LinearSegmentedColormap.from_list("cmap2", ["black", "white", "blue", "red"]), extend="both", levels=np.linspace(var.min(), var.max(), 100)
+            var, cmap=LinearSegmentedColormap.from_list("cmap2", ["black", "white", "blue", "red"]), levels=np.linspace(lim_min, lim_max, 100)
         )
     
     level_value = int(mesonh.level[level])
-    title = f"Simulation Méso-NH du 2022-08-18 à {time} (DX = {resol_dx} m)\nPression au niveau {level} (~{level_value} m) "
+    title = f"Simulation Méso-NH du 2022-08-18 à {time} (DX = {resol_dx} m)\nSomme des rapports de mélange des états condensés\nde l'eau au niveau {level} (~{level_value} m) "
     title = my_map.set_title(title)
     
+    variable = "clouds"
+    label = "Somme des rapports de mélange (g/kg)"
     
     if anim == False:
-        cbar = plt.colorbar(contourf, label="Pression (hPa)")
-        cbar.set_ticks(np.round(np.linspace(var.min(), var.max(), 8)))
+        cbar = plt.colorbar(contourf, label=label)
+        cbar.set_ticks(np.linspace(lim_min, lim_max, 8))
         plt.savefig(f"clouds_lvl{level}_{time}_dx{resol_dx}.png")
 
-    return contourf, title, time, lim_min, lim_max
+    return (contourf, title), time, lim_min, lim_max, variable, label
+
+
+#WATER VAPOR
+def plot_vapor(mesonh, my_map, resol_dx, level, *, anim = False):
+    # # Limits for colorbars
+    with open(f"limits_3D/lim3D_{resol_dx}m.json", "r", encoding="utf-8") as file:
+        lim = json.loads(file.read())
+    lim_min = lim["vapor"][0]
+    lim_max = lim["vapor"][1]
+    
+    var = mesonh.get_var("RVT", level=level)*1000
+    contourf = my_map.plot_contourf(
+            var, cmap=LinearSegmentedColormap.from_list("cmap2", ["black", "white", "blue", "red"]), levels=np.linspace(lim_min, lim_max, 100)
+        )
+    
+    level_value = int(mesonh.level[level])
+    title = f"Simulation Méso-NH du 2022-08-18 à {time} (DX = {resol_dx} m)\nRapport de mélange de vapeur d'eau au niveau {level} (~{level_value} m) "
+    title = my_map.set_title(title)
+    
+    variable = "vapor"
+    label = "Somme des rapports de mélange (g/kg)"
+    
+    if anim == False:
+        cbar = plt.colorbar(contourf, label=label)
+        cbar.set_ticks(np.linspace(lim_min, lim_max, 8))
+        plt.savefig(f"vapor_lvl{level}_{time}_dx{resol_dx}.png")
+
+    return (contourf, title), time, lim_min, lim_max, variable, label
+
+
+#HORIZONTAL WIND
+def norm_wind(ut, vt):
+    return np.sqrt(ut**2 + vt**2)
+
+def norm_quiver(ut, vt):
+    norm = norm_wind(ut, vt)
+    return ut / norm, vt / norm
+
+def plot_horiz_wind(mesonh, my_map, resol_dx, level, *, anim = False):
+    # # Limits for colorbars
+    
+    with open(f"limits_3D/lim3D_{resol_dx}m.json", "r", encoding="utf-8") as file:
+        lim = json.loads(file.read())
+    lim_min = lim["horiz_wind"][0]
+    lim_max = lim["horiz_wind"][1]
+    
+    var = mesonh.get_var("UT", "VT", func=norm_wind, level=level)*3.6
+    contourf = my_map.plot_contourf(
+            var, cmap=LinearSegmentedColormap.from_list("cmap2",
+                [
+                    "white",
+                    (240 / 255, 248 / 255, 255 / 255),
+                    "darkcyan",
+                    "yellow",
+                    "orange",
+                    "red",
+                    "purple",
+                    "black",
+                ]), levels=np.linspace(lim_min, lim_max, 100)
+        )
+    mesh=20
+    kwargs = {
+        "x_mesh": mesh,
+        "y_mesh": mesh,
+        "width": 0.015,
+        "scale": 7,
+        "scale_units": "xy",
+        "units": "xy",
+    }
+    UT_norm, VT_norm = mesonh.get_var("UT", "VT", func=norm_quiver, level=level)
+    quiver = my_map.plot_quiver(UT_norm, VT_norm, **kwargs)
+    
+    level_value = int(mesonh.level[level])
+    title = f"Simulation Méso-NH du 2022-08-18 à {time} (DX = {resol_dx} m)\nNorme et direction du vent horizontal au niveau {level} (~{level_value} m) "
+    title = my_map.set_title(title)
+    
+    variable = "horiz_wind"
+    label = "Norme du vent horizontal (km/h)"
+    
+    if anim == False:
+        cbar = plt.colorbar(contourf, label=label)
+        cbar.set_ticks(np.linspace(lim_min, lim_max, 8))
+        plt.savefig(f"{variable}_lvl{level}_{time}_dx{resol_dx}.png")
+
+    return (contourf, title, quiver), time, lim_min, lim_max, variable, label
+
+
+#VERTICAL WIND
+def plot_vert_wind(mesonh, my_map, resol_dx, level, *, anim = False):
+    # # # Limits for colorbars
+    # with open(f"limits_3D/lim3D_{resol_dx}m.json", "r", encoding="utf-8") as file:
+    #     lim = json.loads(file.read())
+    # lim_min = lim["vert_wind"][0]
+    # lim_max = lim["vert_wind"][1]
+    lim_min = -15
+    lim_max = 30
+    
+    var = mesonh.get_var("WT", level=level)
+    contourf = my_map.plot_contourf(
+            var, cmap="seismic", norm=TwoSlopeNorm(0), extend='both', levels=np.linspace(lim_min, lim_max, 100)
+        )
+    
+    level_value = int(mesonh.level[level])
+    title = f"Simulation Méso-NH du 2022-08-18 à {time} (DX = {resol_dx} m)\nNorme du vent vertical au niveau {level} (~{level_value} m) "
+    title = my_map.set_title(title)
+    
+    variable = "vert_wind"
+    label = "Norme du vent vertical (m/s)"
+    
+    if anim == False:
+        cbar = plt.colorbar(contourf, label=label)
+        cbar.set_ticks(np.linspace(lim_min, lim_max, 8))
+        plt.savefig(f"{variable}_lvl{level}_{time}_dx{resol_dx}.png")
+
+    return (contourf, title), time, lim_min, lim_max, variable, label
 
 
 def plot_anim(mesonh, my_map, resol_dx, func):
-    frame = []
+    print(mesonh.get_limits("PABST")) #/100
+    print(mesonh.get_limits("RCT", "RIT", "RGT", "RST", func=sum_clouds)) #*1000
+    print(mesonh.get_limits("RVT")) #*1000
+    print(mesonh.get_limits("UT", "VT", func=norm_wind)) #*3.6
+    print(mesonh.get_limits("WT"))
     
+    frame = []
     for level in range(0, len(mesonh.level)):
-        #print(mesonh.get_limits("PABST"))
-        contourf, title, time, lim_min, lim_max = func(mesonh, my_map, resol_dx, level, anim = True)
+        frame_content, time, lim_min, lim_max, variable, label = func(mesonh, my_map, resol_dx, level, anim = True)
         
-        frame.append([contourf, title])
+        frame.append(frame_content)
         
-    cbar = plt.colorbar(contourf, label="Pression (hPa)")
-    cbar.set_ticks(np.round(np.linspace(lim_min, lim_max, 8)))
+    cbar = plt.colorbar(frame_content[0], label=label)
+    cbar.set_ticks(np.linspace(lim_min, lim_max, 8))
     
     animation = ArtistAnimation(fig, frame, interval=250)
-    animation.save(f"pressure_anim_{time}_dx{resol_dx}.gif")
+    animation.save(f"{variable}_anim_{time}_dx{resol_dx}.gif")
     
     
-resol_dx = 1000
-level = 80
+resol_dx = 250
+level = 63
 index_echeance = 2
 
 
@@ -112,13 +248,7 @@ lat = [0, 0]  # bornes min max lat
 lon[0], lat[0] = index_to_lonlat(mesonh, i_lim[0], j_lim[0])
 lon[1], lat[1] = index_to_lonlat(mesonh, i_lim[1], j_lim[1])
 
-# # Limits for colorbars
-with open(f"limits_3D/lim3D_{resol_dx}m.json", "r", encoding="utf-8") as file:
-    lim = json.loads(file.read())
-lim_min = lim["pressure"][0]
-lim_max = lim["pressure"][1]
-
-fig, axes, _ = my_map.init_axes(fig_kw={"figsize": (8, 5), "layout": "compressed"})
+fig, axes, _ = my_map.init_axes(fig_kw={"figsize": (8, 5), "layout": "compressed"}, feature_kw={"color" : "gray"})
 axes.set_extent([lon[0], lon[1], lat[0], lat[1]])
 
 
@@ -126,4 +256,13 @@ axes.set_extent([lon[0], lon[1], lat[0], lat[1]])
 #plot_anim(mesonh, my_map, resol_dx, plot_pressure)
 
 #plot_clouds(mesonh, my_map, resol_dx, level, )
-plot_anim(mesonh, my_map, resol_dx, plot_clouds)
+#plot_anim(mesonh, my_map, resol_dx, plot_clouds)
+
+#plot_vapor(mesonh, my_map, resol_dx, level, )
+#plot_anim(mesonh, my_map, resol_dx, plot_vapor)
+
+#plot_horiz_wind(mesonh, my_map, resol_dx, level, )
+#plot_anim(mesonh, my_map, resol_dx, plot_horiz_wind)
+
+#plot_vert_wind(mesonh, my_map, resol_dx, level, )
+#plot_anim(mesonh, my_map, resol_dx, plot_vert_wind)
